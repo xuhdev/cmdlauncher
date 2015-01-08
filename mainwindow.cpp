7cfbf3283a480ecd8e565005e3f65cb23bde644b
Hong Xu <hong@topbug.net>
2015-01-08 00:05:40 -0800

    Update all examples, and fix a minor bug.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,12 +10,12 @@
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or (at your option)
  * any later version.
- * 
+ *
  * CmdLauncher is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with CmdLauncher. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -123,7 +123,7 @@ MainWindow::MainWindow(QWidget *parent)
         {
             QComboBox* new_combobox =
                     new QComboBox(ui.mainTableViews[tabpage]);
-            new_combobox->addItems(item->value("list").toStringList());
+            new_combobox->addItems(item->value("list").toString().split(','));
             new_combobox->setCurrentIndex(item->value("default").toInt());
             new_widget = new_combobox;
         }
